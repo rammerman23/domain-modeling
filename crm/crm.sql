107ce88bea069b6d73cf26dd41a4ab376b55ac7f
rammerman23 <richard.ammerman@kellogg.northwestern.edu>
2022-01-19 03:11:27 +0000

    in class exercises

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,55 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS company_contacts;
+DROP TABLE IF EXISTS company_industries;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS salespersons;
+
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  address TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  position TEXT,
+  email TEXT,
+  phone_number TEXT,
+  address TEXT,
+  company_id INTEGER
+);
+
+CREATE TABLE industries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE industry_membership (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  industry_id INTEGER
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  action TEXT,
+  time TEXT,
+  notes TEXT,
+  salesperson_id INTEGER,
+  contact_id INTEGER
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,60 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  username TEXT,
+  email TEXT,
+  address TEXT,
+);
+
+CREATE TABLE followers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  followed_user_id INTEGER
+  follower_id INTEGER
+);
+
+CREATE TABLE followed (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  followed_user_id INTEGER
+  follower_id INTEGER
+);
+
+CREATE TABLE photos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  file_name TEXT
+  uploaded_time TEXT
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  photo_id INTEGER,
+  caption TEXT,
+  created_on TEXT,
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER,
+  created_on TEXT,
+  active INTEGER
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  post_id INTEGER,
+  created_on TEXT,
+  message TEXT,
+);
+
+CREATE TABLE feed (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  followed_user_id INTEGER
+);
+
 
